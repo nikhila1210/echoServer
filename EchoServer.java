16b71b0e78fae6efb9bacdd6862362f01989e2f5
anil <anilkothattil@gmail.com>
2018-04-03 13:28:18 +0530

    serve function edited

--- EchoServer.java
+++ EchoServer.java
@@ -14,4 +14,31 @@ public class EchoServer
 			System.out.println(err);
 		}
 	}
-
+public void serve()
+	{
+		try
+		{
+			while (true)
+			{
+				Socket client = server.accept();
+				BufferedReader r = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				PrintWriter w = new PrintWriter(client.getOutputStream(), true);
+				w.println("Welcome to the Java EchoServer.  Type 'bye' to close.");
+				String line;
+				do
+				{
+					line = r.readLine();
+					if ( line != null ){
+						w.println("Server: "+ line);	
+						System.out.println("Client says : "+line);		
+					}
+				}
+				while ( !line.trim().equals("bye") );
+				client.close();
+			}
+		}
+		catch (Exception err)
+		{
+			System.err.println(err);
+		}
+	}
