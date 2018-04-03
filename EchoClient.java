2737d21a3a75404d878e910fad2d5bf6add93910
anilagraceprakash <anilagracep@gmail.com>
2018-04-03 13:38:43 +0530

    Client file modified

--- EchoClient.java
+++ EchoClient.java
@@ -8,3 +8,7 @@ public class EchoClient
 		try
 		{
 			Socket s = new Socket("127.0.0.1", 9999);
+                        BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			PrintWriter w = new PrintWriter(s.getOutputStream(), true);
+			BufferedReader con = new BufferedReader(new InputStreamReader(System.in));
+			String line;
