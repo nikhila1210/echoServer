6ac52160d5a0ddfb8655e1234e01bdee3038b174
anil <anilkothattil@gmail.com>
2018-04-03 13:18:32 +0530

    Echo Server started

---
+++ EchoServer.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+
+public class EchoServer
+{
+	public EchoServer(int portnum)
+	{
+		try
+		{
+			server = new ServerSocket(portnum);
+		}
+		catch (Exception err)
+		{
+			System.out.println(err);
+		}
+	}
+
